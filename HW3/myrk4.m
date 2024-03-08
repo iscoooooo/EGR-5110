abffa8c71188374522b7e01a0a1cbd5ede779d97
Francisco Sanudo <francisco.sanudo10@gmail.com>
2024-03-07 18:28:11 -0800

    Initialized a file for 4th stage Runge-Kutta solver

---
+++ HW3/myrk4.m
@@ -0,0 +1,13 @@
+function [x,y,vx,vy,t] = myrk4(t0,tf,dt,N,x0,y0,vx0,vy0,fd,tol)
+
+% <Place your solver here>
+
+% <After your solver is finished and you know the trajectory and velocity, write code
+% that will plot trajectory and phase space on two subplots. Add labels so the user knows
+% what the axes represent.>
+
+
+% <If you want to get fancy, you can create an animation of the spaceship flying around
+% the earth and moon for extra credit (see below).>
+
+end
