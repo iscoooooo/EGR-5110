f44e5126cf5deb35548bea73814e57fc259a7615
Francisco Sanudo <francisco.sanudo10@gmail.com>
2024-03-18 00:07:36 -0700

    Changed initial conditions

--- HW3/MAIN.m
+++ HW3/MAIN.m
@@ -4,15 +4,17 @@ clc; clear; close all;
 
 x0 = 1.2; y0 = 0;          % initial position of spaceship (normalized units)
 vx0 = 0; vy0 = -1.0493571; % initial velocity of spaceship (normalized units)
-t0  = 0; tf = 10;          % initial time, final time (normalized units)
-fd  = 0;                   % deceleration coefficient
-N   = 10000;               % initial guess for number of time steps
+t0  = 0; tf = 4;          % initial time, final time (normalized units)
+fd  = 1;                   % deceleration coefficient
+N   = 1000;               % initial guess for number of time steps
 dt  = (tf-t0)/N;           % dt = time step (normalized units)
 tol = 0.01;                % tolerance used for termination criteria (for extra credit)
 
 % I will adjust the parameters above when grading your assignment
 
+tic
 [x,y,vx,vy,t] = myrk4(t0,tf,dt,N,x0,y0,vx0,vy0,fd,tol);
+toc
 % You will need to write an RK4 solver in a separate M-file named myrk4.m
 % x and y are the coordinates of the spaceship at each time step
 % vx and vy are the x and y components of the speed at each time step
